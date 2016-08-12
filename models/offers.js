fbd7ef077391cff42a9d4db540ce2482bad896b9
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-08-12 17:28:51 +0300

    discounting changes

--- models/offers.js
+++ models/offers.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var mongoose = require('mongoose');
-var AssetSchema = require('./assets').AssetSchema;
 var Round = require('fin-rounds').Round;
 
 // Creating rounding function with banker's algorithm and
 // 2-digits after coma precision
 var round = new Round('bank', 2);
+var roundPercents = new Round('bank', 6);
 
 var Schema = mongoose.Schema;
 
@@ -15,16 +17,35 @@ var SellOfferSchema = new Schema({
   titleDocumentID: {type: String, required: true},
   assetClass: {type: String, required: true}
   bookValue: {type: Number, required: true},
+  bookDate: {type: Date, required: true},
   minPriceRate: {type: Number, required: true},
   maxSharedAPR: {type: Number, required: true},
   loanPartRatio: {type: Number, required: true},
-  devidable: {type: Boolean, 'default': true},
+  dividable: {type: Boolean, 'default': true},
   status: {type: String, enum: OfferStatusEnum, required: true},
   expared: {type: Date, required: true},
   updated: {type: Date, `default`: Date.now},
-  relativeFlows: [{type: {value: Number, date: Date}, required: true}]
+  relativeFlows: [{type: {value: Number, date: Date}, required: true}],
+  cachedPrices: Schema.Types.Mixed
 });
 
+SellOfferSchema.methods.getPriceRates = function(rates) {
+  let prices = {};
+  var rateNames = Object.keys(rates);
+
+  var dcf = discount(this.relativeFlows).on(this.bookDate);
+
+  this.cachedPrices = this.cachedPrices || {};
+
+  for (let rateName of rateNames) {
+    let cacheName = roundPercents(rates[rateName] * 10000).toString();
+    if (!this.cachedPrices[cacheName]) {
+      this.cachedPrices[cacheName] = dcf.by(rates[rateName]);
+    }
+    prices[rateName] = this.cachedPrices[cacheName];
+  }
+
+};
 
 var BuyOfferSchema = new Schema({
   assetClasses: {type: [String], required: true},
@@ -33,7 +54,7 @@ var BuyOfferSchema = new Schema({
   intermediaryMarginRate: {type: Number, required: true},
   maxPriceRate: {type: Number, required: true},
   maxInvestmentPerLoan: Number,
-  buyWholeLoan: {type: Boolean, 'default': false},
+  buyWholeAsset: {type: Boolean, 'default': false},
   status: {type: String, enum: OfferStatusEnum, required: true},
   expared: {type: Date, required: true},
   updated: {type: Date, `default`: Date.now},
@@ -42,40 +63,49 @@ var BuyOfferSchema = new Schema({
 
 var CommitmentStatusEnum = ['blank', 'made', 'executed'];
 var CommitmentSchema = new Schema({
-  bookValue: {type: Number, required: true}, // the amount of asset book value to be sold/purhcased
-  investment: {type: Number, required: true}, // the amount of investment to be withdrawn from Buyers Account
-  assetPrice: {type: Numver, required: true}, // the amount of assetPrice to be paid to Seller
-  intermediaryMargin: {type: Number, required: true}, // the amount of margin to be left for intermediary
+  bookValue: {type: Number, required: true, default: 0}, // the amount of asset book value to be sold/purhcased
+  investment: {type: Number, required: true, default: 0}, // the amount of investment to be withdrawn from Buyers Account
+  assetPrice: {type: Numver, required: true, default: 0}, // the amount of assetPrice to be paid to Seller
+  intermediaryMargin: {type: Number, required: true, default: 0}, // the amount of margin to be left for intermediary
   status: {type: String, rquired: true, enum: CommitmentStatusEnum}
 });
 
 CommitmentSchema.statics.create = function(sellOffer, buyOffer, callback) {
-  var s = sellOffer;
-  var b = buyOffer;
-  var c = new Commitment({status: 'blank'});
+  let s = sellOffer;
+  let b = buyOffer;
+  let c = new Commitment({status: 'blank'});
 
-  // Identify case when we have to deal with whole loan
-  var buyWholeLoan = (
-    (s.loanPartRatio === 1) && // if asset is whole
-    (b.buyWholeLoan || // if buyer whants to buy whole loan
-     b.maxInvestmentPerLoan >= s.bookValue * b.maxPriceRate // or maxInvestmentPerLoan is enough to guy whole loan
-    )
-  );
-
-  var priceRates = discount(s.relativeFlows).by({
-    apr: b.APR,
-    imr: b.APR + b.intermediaryMarginRate
+  // getting asset prices for intermediary and for investor
+  let price = s.getPriceRates({
+    toSell: b.APR / 365, // investors rate
+    toBuy: (b.APR + b.intermediaryMarginRate) / 365 // investors rate + intermediary margin
   });
 
-  if (buyWholeLoan) {
-    c.bookValue = s.bookValue;
-    c.investment = round(c.bookValue * priceRates.apr);
-    c.assetPrice = round(c.bookValue * priceRates.imr);
-  } else {
+  // Identify case when we have to deal with whole loan
+  var buyWholeAsset =
+    (s.loanPartRatio === 1) && // if asset is whole AND
+    (b.buyWholeAsset) // if buyer whants to buy whole loan
+  ;
 
+  if (buyWholeAsset || !s.dividable) {
+    c.bookValue = s.bookValue;
+  } else {
+    c.bookValue = round(b.maxInvestmentPerLoan / price.toSell);
+    if (c.bookValue - s.bookValue > 0) c.bookValue = s.bookValue;
+  }
+  c.investment = round(c.bookValue * price.toSell);
+
+  if (c.investment - b.volume > 0) {
+    if (!s.dividable) {
+      throw new Error('Not enough investment funds to buy asset');
+    }
+    c.investment = b.volume;
+    c.bookValue = round(c.investment / price.toSell);
   }
 
-  return c;
+  c.assetPrice = round(c.bookValue * price.toBuy);
+  c.intermediaryMargin = round(c.investment - c.assetPrice);
+  return c.save(callback);
 };
 
 var SellOffer = mongoose.model('SellOffer', SellOfferSchema);
@@ -92,30 +122,26 @@ function discount(flows) {
 
 function DCF(flows) {
   this.flows = flows;
+  this.startFrom = null;
 };
 
-DCF.prototype.by = function (rates, d0) {
-  d0 = d0 || Date.now();
-  if (typeof(rates) === 'number') {
-    rates = {$: rates}
-  };
-  const rs = Object.keys(rates);
-  const results = rs.reduce((rateName, res) => {
-    res[rateName] = 0;
-    return res;
-  }, {});
-
-  for (let flow of this.flows) {
-    let pwIndex = (flow.date.valueOf() - d0) / 365;
-    for (let dr of rs) {
-      results[dr] += flow.value / Math.pow(1 + rates[dr], pwIndex)
-    }
-  };
-
-  if (rs.length === 1 && typeof(results.$) !== 'undefined') {
-    return results.$;
-  }
-
-  return results;
-
+DCF.prototype.on = function(date) {
+  this.startFrom = new Date(date);
+  return this;
 }
+
+DCF.prototype.by = function (rate) {
+  let d0 = this.startFrom || new Date();
+  rate = parseFloat(rate) + 1;
+  return this.flows.reduce((p, S) => S += p / Math.pow(rate, days(p.date, d0)), 0)
+}
+
+
+function days(date1, date2) {
+  let ms = date1.valueOf();
+  if (typeof(date2) !== 'undefined') {
+    ms -= date2.valueOf();
+  }
+  return parseInt(ms / days.msPerDay, 10);
+}
+days.msPerDay = 1000 * 60 * 60 * 24;
